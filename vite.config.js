84b6c18f1d0e1abc49d032405799d7783ee7ebe2
Jan Sůra <info@jsuradesign.cz>
2023-02-17 23:31:05 +0100

    Edit html files and added new components and sections templates

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     base: "./",
     root: resolve(__dirname, "src"),
     build: {
-        minify: false,
+        minify: "terser",
         outDir: resolve(__dirname, "dist"),
         sourcemap: true,
         copyPublicDir: true,
@@ -46,8 +46,91 @@ export default defineConfig({
             variables: {
                 "index.html": {
                     siteName: "Jan Sůra",
+                    siteDescription: "WebKodér & WebDesignér",
                     version: pkg.version,
-                    repoUrl: pkg.repository.url
+                    repoUrl: pkg.repository.url,
+                    jobs: [
+                        {
+                            id: "01",
+                            company: "Nausus, s.r.o.",
+                            duration: "06/2016 - 08/2017",
+                            position: "HTML & CSS kodér / Web designér",
+                            url: "",
+                            icons: [
+                                {
+                                    file: "design-square"
+                                },
+                                {
+                                    file: "code-square"
+                                }
+                            ]
+                        },
+                        {
+                            id: "02",
+                            company: "FaceUp, s.r.o.",
+                            duration: "11/2017 - 05/2019",
+                            position: "HTML & CSS kodér / Web designér",
+                            url: "",
+                            icons: [
+                                {
+                                    file: "design-square"
+                                },
+                                {
+                                    file: "code-square"
+                                }
+                            ]
+                        },
+                        {
+                            id: "03",
+                            company: "Česká televize",
+                            duration: "06/2019 - 01/2023",
+                            position: "Web UI designér / Grafik",
+                            url: "",
+                            icons: [
+                                {
+                                    file: "design-square"
+                                }
+                            ]
+                        },
+                        {
+                            id: "04",
+                            company: "Česká televize",
+                            duration: "01/2023 - do současnosti",
+                            position: "Junior FrontEnd developer",
+                            url: "",
+                            icons: [
+                                {
+                                    file: "code-square"
+                                }
+                            ]
+                        }
+                    ],
+                    socialLinks: [
+                        {
+                            id: "01",
+                            title: "GitHub",
+                            url: "https://github.com/jsuradesign/",
+                            icon: "github"
+                        },
+                        {
+                            id: "02",
+                            title: "CodePen",
+                            url: "",
+                            icon: "codepen"
+                        },
+                        {
+                            id: "03",
+                            title: "LinkedIn",
+                            url: "",
+                            icon: "linkedin"
+                        },
+                        {
+                            id: "04",
+                            title: "Facebook",
+                            url: "",
+                            icon: "facebook"
+                        }
+                    ]
                 }
             }
         }),
