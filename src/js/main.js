c1e67e340189b7665a0aa01082858f061cfcddf1
Jan Sůra <info@jsuradesign.cz>
2023-02-20 00:08:46 +0100

    Added new image, importing MenuToggle.js to main file

--- src/js/main.js
+++ src/js/main.js
@@ -3,9 +3,11 @@ import "../scss/main.scss";
 import ThemeSwitcher from "./components/ThemeSwitcher";
 import BlureHeroImage from "./components/BlurHeroImage";
 import CookiesConsent from "./cookieconsent-init";
+import MenuToggle from "./components/MenuToggle";
 
 new CookiesConsent;
 new ThemeSwitcher;
+new MenuToggle;
 new BlureHeroImage;
 
 const copyYear = document.querySelector("#copy-year");
