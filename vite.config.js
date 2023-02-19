04827245081b2d2663a5860ddc8bf5c7b3e29fbb
Jan Sůra <info@jsuradesign.cz>
2023-02-19 02:40:22 +0100

    Update nunjuck variables

--- vite.config.js
+++ vite.config.js
@@ -9,14 +9,17 @@ export default defineConfig({
     base: "./",
     root: resolve(__dirname, "src"),
     build: {
-        minify: "terser",
+        minify: false, // "terser"
         outDir: resolve(__dirname, "dist"),
         sourcemap: true,
+        manifest: true,
         copyPublicDir: true,
         reportCompressedSizeLimit: true,
         rollupOptions: {
+            external: ["vanilla-cookieconsent"],
             input: {
                 index: resolve(__dirname, "src/index.html"),
+                cookieconsent: resolve(__dirname, "src/js/cookieconsent-init.js"),
             },
             output: {
                 assetFileNames: (assetInfo) => {
@@ -49,12 +52,31 @@ export default defineConfig({
                     siteDescription: "WebKodér & WebDesignér",
                     version: pkg.version,
                     repoUrl: pkg.repository.url,
+                    links: [
+                        {
+                            title: "Úvod",
+                            link: "uvod"
+                        },
+                        {
+                            title: "Zkušenosti",
+                            link: "zkusenosti"
+                        },
+                        {
+                            title: "Dovednosti",
+                            link: "dovednost"
+                        },
+                        {
+                            title: "Kontakt",
+                            link: "kontakt"
+                        }
+                    ],
                     jobs: [
                         {
                             id: "01",
                             company: "Nausus, s.r.o.",
                             duration: "06/2016 - 08/2017",
                             position: "HTML & CSS kodér / Web designér",
+                            logo: "logo-nausus.png",
                             url: "",
                             icons: [
                                 {
@@ -70,6 +92,7 @@ export default defineConfig({
                             company: "FaceUp, s.r.o.",
                             duration: "11/2017 - 05/2019",
                             position: "HTML & CSS kodér / Web designér",
+                            logo: "logo-fup.png",
                             url: "",
                             icons: [
                                 {
@@ -85,6 +108,7 @@ export default defineConfig({
                             company: "Česká televize",
                             duration: "06/2019 - 01/2023",
                             position: "Web UI designér / Grafik",
+                            logo: "logo-ceska_televize.png",
                             url: "",
                             icons: [
                                 {
@@ -97,6 +121,7 @@ export default defineConfig({
                             company: "Česká televize",
                             duration: "01/2023 - do současnosti",
                             position: "Junior FrontEnd developer",
+                            logo: "logo-ceska_televize.png",
                             url: "",
                             icons: [
                                 {
@@ -115,24 +140,35 @@ export default defineConfig({
                         {
                             id: "02",
                             title: "CodePen",
-                            url: "",
+                            url: "https://codepen.io/jSura",
                             icon: "codepen"
                         },
                         {
                             id: "03",
                             title: "LinkedIn",
-                            url: "",
+                            url: "https://www.linkedin.com/in/jansura/",
                             icon: "linkedin"
                         },
                         {
                             id: "04",
                             title: "Facebook",
-                            url: "",
+                            url: "https://www.facebook.com/jan.sura.3",
                             icon: "facebook"
                         }
                     ]
                 }
             }
         }),
+        {
+            name: "inject",
+            transformIndexHtml() {
+                return [
+                    {
+                        tag: "script",
+                        attrs: { type: 'module', src: '/external.js', defer: true },
+                    }
+                ]
+            }
+        }
     ]
 });
