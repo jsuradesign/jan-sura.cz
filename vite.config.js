8f0e240fe24c010457bd4532f07e152d6d867147
Jan Sůra <info@jsuradesign.cz>
2023-02-19 20:55:25 +0100

    Move nunjucks variables to separate data file

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { resolve } from "path"
+import fs from "fs"
 import { defineConfig } from "vite"
 
 import nunjucks from "vite-plugin-nunjucks"
 
 import pkg from "./package.json"
+
+let data = fs.readFileSync("./src/data/data.json");
+let parseData = JSON.parse(data);
 
 export default defineConfig({
     base: "./",
@@ -46,129 +50,8 @@ export default defineConfig({
     plugins: [
         nunjucks({
             templatesDir: resolve(__dirname, "src/html"),
-            variables: {
-                "index.html": {
-                    siteName: "Jan Sůra",
-                    siteDescription: "WebKodér & WebDesignér",
-                    version: pkg.version,
-                    repoUrl: pkg.repository.url,
-                    links: [
-                        {
-                            title: "Úvod",
-                            link: "uvod"
-                        },
-                        {
-                            title: "Zkušenosti",
-                            link: "zkusenosti"
-                        },
-                        {
-                            title: "Dovednosti",
-                            link: "dovednost"
-                        },
-                        {
-                            title: "Kontakt",
-                            link: "kontakt"
-                        }
-                    ],
-                    jobs: [
-                        {
-                            id: "01",
-                            company: "Nausus, s.r.o.",
-                            duration: "06/2016 - 08/2017",
-                            position: "HTML & CSS kodér / Web designér",
-                            logo: "logo-nausus.png",
-                            url: "",
-                            icons: [
-                                {
-                                    file: "design-square"
-                                },
-                                {
-                                    file: "code-square"
-                                }
-                            ]
-                        },
-                        {
-                            id: "02",
-                            company: "FaceUp, s.r.o.",
-                            duration: "11/2017 - 05/2019",
-                            position: "HTML & CSS kodér / Web designér",
-                            logo: "logo-fup.png",
-                            url: "",
-                            icons: [
-                                {
-                                    file: "design-square"
-                                },
-                                {
-                                    file: "code-square"
-                                }
-                            ]
-                        },
-                        {
-                            id: "03",
-                            company: "Česká televize",
-                            duration: "06/2019 - 01/2023",
-                            position: "Web UI designér / Grafik",
-                            logo: "logo-ceska_televize.png",
-                            url: "",
-                            icons: [
-                                {
-                                    file: "design-square"
-                                }
-                            ]
-                        },
-                        {
-                            id: "04",
-                            company: "Česká televize",
-                            duration: "01/2023 - do současnosti",
-                            position: "Junior FrontEnd developer",
-                            logo: "logo-ceska_televize.png",
-                            url: "",
-                            icons: [
-                                {
-                                    file: "code-square"
-                                }
-                            ]
-                        }
-                    ],
-                    socialLinks: [
-                        {
-                            id: "01",
-                            title: "GitHub",
-                            url: "https://github.com/jsuradesign/",
-                            icon: "github"
-                        },
-                        {
-                            id: "02",
-                            title: "CodePen",
-                            url: "https://codepen.io/jSura",
-                            icon: "codepen"
-                        },
-                        {
-                            id: "03",
-                            title: "LinkedIn",
-                            url: "https://www.linkedin.com/in/jansura/",
-                            icon: "linkedin"
-                        },
-                        {
-                            id: "04",
-                            title: "Facebook",
-                            url: "https://www.facebook.com/jan.sura.3",
-                            icon: "facebook"
-                        }
-                    ]
-                }
-            }
+            // parseData
+            variables: parseData
         }),
-        {
-            name: "inject",
-            transformIndexHtml() {
-                return [
-                    {
-                        tag: "script",
-                        attrs: { type: 'module', src: '/external.js', defer: true },
-                    }
-                ]
-            }
-        }
     ]
 });
